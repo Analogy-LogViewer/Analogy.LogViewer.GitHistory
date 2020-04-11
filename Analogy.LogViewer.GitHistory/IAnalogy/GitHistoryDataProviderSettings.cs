44088929d630e6a81f14d5a8790a4766ddaca453
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-04-11 16:55:10 +0300

    update interfaces

--- Analogy.LogViewer.GitHistory/IAnalogy/GitHistoryDataProviderSettings.cs
+++ Analogy.LogViewer.GitHistory/IAnalogy/GitHistoryDataProviderSettings.cs
@@ -17,7 +17,8 @@ namespace Analogy.LogViewer.GitHistory.IAnalogy
         public UserControl DataProviderSettings { get; }=new GitRepositoriesSettings();
         public Image SmallImage { get; }
         public Image LargeImage { get; }
-        public Guid FactoryId { get; } = GitHistoryFactory.Id;
+        public Guid FactoryId { get; set; } = GitHistoryFactory.Id;
+        public Guid ID { get; set; } = new Guid("AEE6AA94-A0B6-4313-8C65-D81943B542DB");
 
         public Task SaveSettingsAsync()
         {
