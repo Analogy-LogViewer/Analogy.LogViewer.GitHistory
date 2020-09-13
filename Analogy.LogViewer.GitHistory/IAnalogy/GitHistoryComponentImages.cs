c11a5f3e8bd52d3ad17ac0d30fac140d6dba8ecb
Lior Banai <liorb@kama-medical.com>
2020-09-13 10:03:49 +0300

    update nuget

--- Analogy.LogViewer.GitHistory/IAnalogy/GitHistoryComponentImages.cs
+++ Analogy.LogViewer.GitHistory/IAnalogy/GitHistoryComponentImages.cs
@@ -5,28 +5,28 @@ using Analogy.Interfaces;
 
 namespace Analogy.LogViewer.GitHistory.IAnalogy
 {
-    public class GitHistoryComponentImages : IAnalogyComponentImages
-    {
-        public Image GetLargeImage(Guid analogyComponentId)
-        {
-            if (analogyComponentId == GitHistoryFactory.Id)
-                return Resources.Git_icon_32x32;
-            return null;
-        }
+    //public class GitHistoryComponentImages : IAnalogyComponentImages
+    //{
+    //    public Image GetLargeImage(Guid analogyComponentId)
+    //    {
+    //        if (analogyComponentId == GitHistoryFactory.Id)
+    //            return Resources.Git_icon_32x32;
+    //        return null;
+    //    }
 
-        public Image GetSmallImage(Guid analogyComponentId)
-        {
-            if (analogyComponentId == GitHistoryFactory.Id)
-                return Resources.Git_icon_16x16;
-            return null;
-        }
+    //    public Image GetSmallImage(Guid analogyComponentId)
+    //    {
+    //        if (analogyComponentId == GitHistoryFactory.Id)
+    //            return Resources.Git_icon_16x16;
+    //        return null;
+    //    }
 
-        public Image GetOnlineConnectedLargeImage(Guid analogyComponentId) => null;
+    //    public Image GetOnlineConnectedLargeImage(Guid analogyComponentId) => null;
 
-        public Image GetOnlineConnectedSmallImage(Guid analogyComponentId) => null;
+    //    public Image GetOnlineConnectedSmallImage(Guid analogyComponentId) => null;
 
-        public Image GetOnlineDisconnectedLargeImage(Guid analogyComponentId) => null;
+    //    public Image GetOnlineDisconnectedLargeImage(Guid analogyComponentId) => null;
 
-        public Image GetOnlineDisconnectedSmallImage(Guid analogyComponentId) => null;
-    }
+    //    public Image GetOnlineDisconnectedSmallImage(Guid analogyComponentId) => null;
+    //}
 }
